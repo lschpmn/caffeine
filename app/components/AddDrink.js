eedc3e7012e903ec84240a26b65c576c419a0319
lschpmn <luis@lmc-productions.com>
2016-07-13 16:09:00 -0500

    Added basic modal with some styling

--- app/components/AddDrink.js
+++ app/components/AddDrink.js
@@ -2,26 +2,55 @@
 
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {View, Text, TouchableNativeFeedback} from 'react-native';
+import {Modal, Text, TouchableNativeFeedback, View} from 'react-native';
 
 class AddDrink extends Component {
   constructor() {
     super();
+    this.state = {
+      showModal: false
+    };
     
-    this.clicked = this.clicked.bind(this);
+    setInterval(() => {
+      this.setState({showModal: false});
+    }, 5000);
+    
+    this.toggleModal = this.toggleModal.bind(this);
   }
   
-  clicked() {
-    console.log('Clicked');
+  toggleModal() {
+    this.setState({
+      showModal: !this.state.showModal
+    });
   }
   
   render() {
     return <TouchableNativeFeedback
       background={TouchableNativeFeedback.Ripple('red', true)}
-      onPress={this.clicked}
-    > 
+      onPress={this.toggleModal}
+    >
       <View style={styles.button}>
         <Text>Add Drink</Text>
+        
+        <Modal
+          onRequestClose={() => {console.log('no close! only open')}}
+          visible={this.state.showModal}
+          transparent={true}
+        >
+  
+          <View style={styles.modalBackground}>
+            <View style={styles.modal}>
+              
+              {/*modal close button*/}
+              <View style={styles.close}>
+                <Text style={styles.closeButton} onPress={this.toggleModal}>Close</Text>
+              </View>
+              
+              <Text>I'm a modal!</Text>
+            </View>
+          </View>
+          
+        </Modal>
       </View>
     </TouchableNativeFeedback>;
   }
@@ -31,6 +60,29 @@ const styles = {
   button: {
     backgroundColor: '#3498db',
     flex: 1
+  },
+  
+  close: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
+  
+  closeButton: {
+    color: 'red'
+  },
+  
+  modalBackground: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    flex: 1,
+    justifyContent: 'center'
+  },
+  
+  modal: {
+    backgroundColor: 'white',
+    padding: 20,
+    margin: 10,
+    borderRadius: 10
   }
 };
 
