5aa0b2fc972bece348ab0d9dd2decbd95a89fca0
lschpmn <luis@lmc-productions.com>
2016-07-17 14:49:41 -0500

    Added basic structure of list item

--- app/components/DrinkList.js
+++ app/components/DrinkList.js
@@ -1,12 +1,12 @@
 'use strict';
 
 import React, {Component} from 'react';
-import {ListView, Text} from 'react-native';
+import {ListView, Text, View} from 'react-native';
 import {connect} from 'react-redux';
 
 class DrinkList extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       ds: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
     };
@@ -15,11 +15,28 @@ class DrinkList extends Component {
   render() {
     const dataSource = this.state.ds.cloneWithRows(this.props.drinks);
     
-    return <ListView 
-      dataSource={dataSource}
-      renderRow={(drink) => <Text>{drink.name}</Text>}
-    />;
+    return <View style={styles.container}>
+      <ListView
+        dataSource={dataSource}
+        renderRow={(drink) => <View style={styles.row}>
+          <Text style={{flex: 1}}>{drink.name}</Text>
+          <Text style={{flex: 1}}>Edit</Text>
+          <Text style={{flex: 1}}>Delete</Text>
+        </View>}
+      />
+    </View>;
   }
 }
+
+const styles = {
+  container: {
+    marginHorizontal: 5
+  },
+  
+  row: {
+    flex: 1,
+    flexDirection: 'row'
+  }
+};
 
 export default connect(({drinks}) => ({drinks}))(DrinkList);
