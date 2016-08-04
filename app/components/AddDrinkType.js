801c2751ed790747cc46426359d3573a165b2535
lschpmn <luis@lmc-productions.com>
2016-08-03 20:01:30 -0500

    Add drink type button and modal now working

--- app/components/AddDrinkType.js
+++ app/components/AddDrinkType.js
@@ -2,33 +2,58 @@
 
 import React, {Component} from 'react';
 import {Text, TouchableNativeFeedback, View} from 'react-native';
+import {connect} from 'react-redux';
 import {primaryColor} from '../lib/COLORS';
+import DrinkTypeModal from '../components/DrinkTypeModal';
 
-export default class AddDrinkType extends Component {
-  constructor() {
-    super();
+class AddDrinkType extends Component {
+  /**
+   * @param {Redux.Dispatch} props.dispatch
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisible: false
+    };
     
-    this.openAddModal = this.openAddModal.bind(this);
+    this.addDrinkType = this.addDrinkType.bind(this);
   }
   
-  openAddModal() {
-    
+  /**
+   * @param {drinkType} drinkType
+   */
+  addDrinkType(drinkType) {
+    this.props.dispatch({
+      type: 'ADD_DRINK_TYPE',
+      drinkType
+    });
   }
   
   render() {
+    const configuredModal = <DrinkTypeModal 
+      closeModal={() => this.setState({modalVisible: false})}
+      index={-1}
+      submit={this.addDrinkType}
+    />;
+    
     return <View style={styles.container}>
       <TouchableNativeFeedback
         background={TouchableNativeFeedback.Ripple('red')}
         delayPressIn={0}
-        onPress={this.openAddModal}
+        onPress={() => this.setState({modalVisible: true})}
       >
         <View style={styles.button}>
           <Text>Add Drink Type</Text>
+          
+          {this.state.modalVisible ? configuredModal : null}
+          
         </View>
       </TouchableNativeFeedback>
     </View>
   }
 }
+
+export default connect()(AddDrinkType);
 
 const styles = {
   container: {
