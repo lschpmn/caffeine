dd00988b7d90185af26f9cc4bc561f519a585362
lschpmn <luis@lmc-productions.com>
2016-08-09 12:43:20 -0500

    Added red color

--- app/lib/COLORS.js
+++ app/lib/COLORS.js
@@ -4,6 +4,8 @@ export const primaryColor = '#3498db';
 
 export const grey = '#95a5a6';
 
+export const red = '#e53935';
+
 export const white = '#ecf0f1';
 
 export default {primaryColor, grey, white};
